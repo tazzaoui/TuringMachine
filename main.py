d25028ddcbf94354df1dd5d8711f45b61ce2b44f
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-04-15 00:41:55 -0400

    Updated Driver

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from transitions import Transition
 from transitions import Direction
 from turing import TuringMachine
 
-#               s    i    s    0     d
-t0 = Transition('0', '1', '2', '1', Direction.LEFT)
-t2 = Transition('2', '1', '2', '1', Direction.LEFT)
+t0 = Transition("0", "1", "2", "6", Direction.RIGHT)
+t2 = Transition("2", "6", "2", "7", Direction.RIGHT)
 
 t0.toggle_start()
 t2.toggle_accept()
 
-states = ['0', '2']
+states = ["0", "2"]
 transitions = [t0, t2]
-initial_state = t0
-accept_state = t2
+initial_state = "0"
+accept_state = "2"
 reject_state = None
-initial_tape = [0,0,0]
+initial_tape = ['1','1','0']
 
 tm = TuringMachine(states, transitions, initial_state, accept_state, reject_state, initial_tape)
+
+tm.print_transitions()
+tm.print_tape()
 tm.execute()
+tm.print_transitions()
+tm.print_tape()
